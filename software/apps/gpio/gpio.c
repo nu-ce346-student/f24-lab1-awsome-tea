1d3792f824a1e3293e5795b9a338d2e007b8a08a
gydanawa <gyda2021@gmail.com>
2024-10-04 21:21:57 -0500

    All checkpoints done

--- software/apps/gpio/gpio.c
+++ software/apps/gpio/gpio.c
@@ -3,7 +3,22 @@
 typedef struct{
   // Step 3:
   // Add register definitions here
+  uint32_t unused_A[(0x504)/4];
+  uint32_t OUT; //0x504
+  uint32_t OUTSET; //0x508
+  uint32_t OUTCLR; //0x50C
+  uint32_t IN; //0x510
+  uint32_t DIR; //0x514
+  uint32_t DIRSET; //0x518
+  uint32_t DIRCLR; //0x51C
+  uint32_t LATCH; //0x520
+  uint32_t DETECTMODE; //0x524
+  uint32_t unused_B[(0x700 - 0x524)/4 -1];
+  uint32_t PIN_CNF[32]; //0x528-0x77C
 } gpio_reg_t;
+
+volatile gpio_reg_t* GPIO_REG_P0 = (gpio_reg_t*)(0x50000000);
+volatile gpio_reg_t* GPIO_REG_P1 = (gpio_reg_t*)(0x50000300);
 
 // Inputs: 
 //  gpio_num - gpio number 0-31 OR (32 + gpio number)
@@ -12,6 +27,28 @@ void gpio_config(uint8_t gpio_num, gpio_direction_t dir) {
   // Implement me
   // This function should configure the pin as an input/output
   // Hint: Use proper PIN_CNF instead of DIR
+  int new_gpio_num = gpio_num;
+  if (gpio_num >= 32) {
+    new_gpio_num = gpio_num - 32;
+
+    if (dir == GPIO_INPUT) {
+      GPIO_REG_P1->PIN_CNF[new_gpio_num] = GPIO_REG_P1->PIN_CNF[new_gpio_num] & 0xFFFFFFFC;
+    }
+    else {
+      GPIO_REG_P1->PIN_CNF[new_gpio_num] = GPIO_REG_P1->PIN_CNF[new_gpio_num] | 0x00000003;
+    }
+  }
+
+  else {
+    if (dir == GPIO_INPUT) {
+    GPIO_REG_P0->PIN_CNF[new_gpio_num] = GPIO_REG_P0->PIN_CNF[new_gpio_num] & 0xFFFFFFFC;
+    }
+    else {
+      GPIO_REG_P0->PIN_CNF[new_gpio_num] = GPIO_REG_P0->PIN_CNF[new_gpio_num] | 0x00000003;
+    }
+  }
+
+  
 }
 
 // Inputs: 
@@ -20,6 +57,21 @@ void gpio_set(uint8_t gpio_num) {
   // Implement me
   // This function should make the pin high
   // It can assume that the pin has already been configured
+  // int temp = 0;
+  // temp = temp << gpio_num;
+  // int mask = 0xFFFFFFFF;
+  // mask = mask & temp;
+  // GPIO_REG_P0->OUT = GPIO_REG_P0->OUT | (1 << gpio_num);
+
+  if (gpio_num >= 32) {
+    int new_gpio_num = gpio_num - 32;
+    GPIO_REG_P1->OUTSET = (1 << new_gpio_num);
+  }
+
+  else {
+    GPIO_REG_P0->OUTSET = (1 << gpio_num);
+  }
+
 }
 
 // Inputs: 
@@ -28,6 +80,20 @@ void gpio_clear(uint8_t gpio_num) {
   // Implement me
   // This function should make the pin low
   // It can assume that the pin has already been configured
+  // int bit_mask = (1 << gpio_num);
+  // bit_mask = ~(bit_mask);
+  // GPIO_REG_P0->OUT = GPIO_REG_P0->OUT & bit_mask;
+
+  int new_gpio_num = gpio_num;
+  if (gpio_num >= 32) {
+    new_gpio_num = gpio_num - 32;
+
+    GPIO_REG_P1->OUTCLR = (1 << new_gpio_num);
+  }
+  else {
+    GPIO_REG_P0->OUTCLR = (1 << new_gpio_num);
+  }
+ 
 }
 
 // Inputs: 
@@ -39,7 +105,37 @@ bool gpio_read(uint8_t gpio_num) {
   // This function should read the value from the pin
   // It can assume that the pin has already been configured
 
-  return true;
+
+  if (gpio_num < 32) {
+    //use port 0
+    int bit_mask = (1 << gpio_num);
+    // printf("%ld\n", GPIO_REG_P0->OUT);
+    // printf("bit_mask = %i\n", bit_mask);
+
+    if ((GPIO_REG_P0->IN & bit_mask) != 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+
+  }
+  else {
+    //use port 1
+    int new_gpio_num = gpio_num - 32;
+    int bit_mask = (1 << new_gpio_num);
+
+    if ((GPIO_REG_P1->IN & bit_mask) != 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+
+
+  }
+
+  
 }
 
 // prints out some information about the GPIO driver. Can be called from main()
@@ -47,5 +143,9 @@ void gpio_print(void) {
   // Use this function for debugging purposes
   // For example, you could print out struct field addresses
   // You don't otherwise have to write anything here
+  printf("%p\n", &(GPIO_REG_P0->OUT));
+  printf("%p\n", &(GPIO_REG_P0->DIR));
+  printf("%p\n", &(GPIO_REG_P0->LATCH));
+  printf("%p\n", &(GPIO_REG_P0->PIN_CNF[0]));
 }
 
--- software/apps/gpio/main.c
+++ software/apps/gpio/main.c
@@ -17,21 +17,100 @@ int main(void) {
 
   // Turn on all LEDs on the back of the Microbit
   // Add code here
+  gpio_print();
+
+  //configure 10 GPIO pins as outputs
+  gpio_config(21, GPIO_OUTPUT);
+  gpio_config(22, GPIO_OUTPUT);
+  gpio_config(15, GPIO_OUTPUT);
+  gpio_config(24, GPIO_OUTPUT);
+  gpio_config(19, GPIO_OUTPUT);
+  gpio_config(28, GPIO_OUTPUT);
+  gpio_config(11, GPIO_OUTPUT);
+  gpio_config(31, GPIO_OUTPUT);
+  gpio_config(37, GPIO_OUTPUT);
+  gpio_config(30, GPIO_OUTPUT);
+
+  //configure the output value
+  //set high all the rows
+  gpio_set(21);
+  gpio_set(22);
+  gpio_set(15);
+  gpio_set(24);
+  gpio_set(19);
+
+  //clear all the columns
+  gpio_clear(28);
+  gpio_clear(11);
+  gpio_clear(31);
+  gpio_clear(37);
+  gpio_clear(30);
 
   // Control LED with raw MMIO
   // Microphone LED is P0.20 and active high
   // Add code here
 
+
+  //CHECK OFF #11 : -------------------------
+  //write DIR register
+  *(uint32_t*)(0x50000514) = 0x100000;
+  //write OUT register
+  *(uint32_t*)(0x50000504) = 0x100000;
+  // ----------------------------------------
+
   // loop forever
+  // printf("Looping\n");
+
+  // printf("Test 1: \n");
+  // gpio_config(20, GPIO_OUTPUT);
+  // gpio_clear(20);
+  // printf("gpio 20 read: %s\n", gpio_read(20) ? "true" : "false");
+  // gpio_set(20);
+  // printf("gpio 20 read: %s\n", gpio_read(20) ? "true" : "false");
+  // gpio_clear(20);
+  // printf("gpio 20 read: %s\n", gpio_read(20) ? "true" : "false");
+  // gpio_set(20);
+  // printf("gpio 20 read: %s\n", gpio_read(20) ? "true" : "false");
+
+
+  // printf("Test 1 complete.\n");
+
+  // printf("Test 2: \n");
+  // gpio_config(31, GPIO_OUTPUT);
+  // printf("gpio 31 read: %s\n", gpio_read(31) ? "true" : "false");
+  // gpio_set(31);
+  // printf("gpio 31 read: %s\n", gpio_read(31) ? "true" : "false");
+  // gpio_clear(31);
+  // printf("gpio 31 read: %s\n", gpio_read(31) ? "true" : "false");
+
+
+  // printf("Test 1 complete.\n");
+
+  //CHECKOFF 13: ------------------
+  //initialize buttons
+  gpio_config(14, GPIO_INPUT); //button A
+  gpio_config(23, GPIO_INPUT); //button B
+  gpio_config(20, GPIO_OUTPUT); //microphone LED
+
   printf("Looping\n");
   while (1) {
+
+    // printf("Looping\n");
+    printf("GPIO 14 (A): %s\n", gpio_read(14) ? "true" : "false");
+    printf("GPIO 23 (B): %s\n", gpio_read(23) ? "true" : "false");
 
     // Control LED with buttons
     // Button A is P0.14 and active low
     // Button B is P0.23 and active low
     // Add code here
 
-    nrf_delay_ms(100);
-  }
+    if (gpio_read(14) == false) {
+      gpio_set(20);
+    }
+    if (gpio_read(23) == false) {
+      gpio_clear(20);
+    }
+    // nrf_delay_ms(10);
+}
 }
 
